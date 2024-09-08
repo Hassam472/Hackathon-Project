22b484a51e8dd7cb09003d6987268763736d2687
Muhammad Hassam Uddin <mhassamuddin125@gmail.com>
2024-09-08 14:53:16 +0900

    final

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-// Get the button and section elements
 const button = document.getElementById('toggle-skills');
 const sections = document.getElementById('skills');
-// Ensure both elements exist before adding the event listener
 if (button && sections) {
     button.addEventListener('click', () => {
         sections.style.visibility = "hidden";
